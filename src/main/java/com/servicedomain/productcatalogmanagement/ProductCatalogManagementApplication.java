b848ce6ed7e00b00e48ddd5fcbad67f35434fe55
Abner Rodrigues <abnerrf@hotmail.com>
2024-05-24 18:23:42 -0300

    feat: First commit - Criação CRUD Products

---
+++ src/main/java/com/servicedomain/productcatalogmanagement/ProductCatalogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.servicedomain.productcatalogmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piggysafe/product_catalog_management/ProductCatalogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggysafe.product_catalog_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
